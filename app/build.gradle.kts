58f156ecf28aec098b6ab9c6a995bf13e8508e98
Rashid Magomedov <Rashid.magomedov.05.dev@gmail.com>
2025-08-19 18:13:16 +0300

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,5 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.androidx.compose.navigation)
 }
